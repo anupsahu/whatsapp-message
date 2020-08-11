f730cb510f3544e9eefe95f74b4d6d71bf5b5853
Anup Sahu <44465538+anupsahu02@users.noreply.github.com>
2020-08-11 12:36:22 +0530

    Adding number support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'whatsapp-message';
+export class AppComponent implements OnInit {
+  numberForm: FormGroup;
+
+  ngOnInit() {
+    this.numberForm = new FormGroup({
+      mobile: new FormControl(null, [
+        Validators.pattern('[0-9]{0-10}'),
+        Validators.required,
+      ]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.numberForm);
+    window.open('https://wa.me/91' + this.numberForm.value.mobile);
+  }
 }
